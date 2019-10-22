be11e2d87883aa052074ee88b9332724c6d8c5c6
guy.golan <guygolan91@gmail.com>
2019-10-22 20:56:34 +0300

    added mongoDb

--- server/app.js
+++ server/app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://guygolanil:Nordia5741376!@cluster0-1dvwl.mongodb.net/test?retryWrites=true&w=majority", err => {
+  if(err) {
+    console.error(err);
+  }
+});
+
+mongoose.connection.once('open', () => {
+  console.log('connected to db');
+});
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
 }));
+
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,7 @@
 const graphql = require('graphql');
+const TaskModel = require('../dbmodels/task');
+const CategoryModel = require('../dbmodels/category');
+
 const { 
     GraphQLObjectType, 
     GraphQLString,
@@ -7,17 +10,6 @@ const {
     GraphQLList
  } = graphql;
 
-const TASKS = [
-    {id: '1', name: 'Learn Redux', description: 'Need to improve my stack', categoryId: '1' },
-    {id: '2', name: 'Learn GraphQl', description: 'Need to know it duh' , categoryId: '1'},
-    {id: '3', name: 'Play Cities Skyline', description: 'Got the dudah' , categoryId: '2'},
-];
-
-const CATEGORIES = [
-    {id: '1', title: 'Programming', details: 'Relates to programs'},
-    {id: '2', title: 'Gaming', details: 'Games and shite'},
-    {id: '3', title: 'Work', details: 'Work stuff'}
-];
 
 const TaskType = new GraphQLObjectType({
     name: 'Task',
@@ -29,7 +21,7 @@ const TaskType = new GraphQLObjectType({
             category: {
                 type: CategoryType,
                 resolve(parent, args){
-                    return CATEGORIES.find(cat => cat.id === parent.categoryId);
+                    return CategoryModel.findById(parent.categoryId);
                 }
             }
         }
@@ -46,7 +38,7 @@ const CategoryType = new GraphQLObjectType({
             tasks: {
                 type: new GraphQLList(TaskType),
                 resolve(parent, args){
-                    return TASKS.filter(task => task.categoryId === parent.id);
+                    return TaskModel.find({ categoryId: parent.id });
                 }
             }
         }
@@ -60,31 +52,68 @@ const RootQuery = new GraphQLObjectType({
             type: TaskType,
             args: {id: {type: GraphQLID } },
             resolve(parent, args){
-                return TASKS.find( task => task.id === args.id);
+                return TaskModel.findById(args.id);
             }
         },
         tasks: {
             type: new GraphQLList(TaskType),
             resolve(parent, args){
-                return TASKS;
+                return TaskModel.find({});
             }
         },
         category: {
             type: CategoryType,
             args: {id: {type: GraphQLID}},
             resolve(parent, args){
-                return CATEGORIES.find(category => category.id === args.id);
+                return CategoryModel.findById(args.id);
             }
         },
         categories: {
             type: new GraphQLList(CategoryType),
             resolve(parent, args) {
-                return CATEGORIES;
+                return CategoryModel.find({});
+            }
+        }
+    }
+});
+
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addTask: {
+            type: TaskType,
+            args: {
+                name: { type: GraphQLString },
+                description: {type: GraphQLString },
+                categoryId: {type: GraphQLID }
+            },
+            resolve(parent, args) {
+                const task = new TaskModel({
+                    name: args.name,
+                    description: args.description,
+                    categoryId: args.categoryId
+                });
+                return task.save();
+            }
+        },
+        addCategory: {
+            type: CategoryType,
+            args: {
+                title: { type: GraphQLString },
+                details: {type: GraphQLString }
+            },
+            resolve(parent, args) {
+                const category = new CategoryModel({
+                    title: args.title,
+                    details: args.details
+                });
+               return category.save();
             }
         }
     }
 });
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 });
